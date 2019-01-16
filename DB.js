fbc44e28e86b6c4aa9c4a4e55d7a6feda0ac4811
zhiliv <zhiliv@gmail.com>
2019-01-16 08:25:46 +0300

    initial

--- DB.js
+++ DB.js
@@ -2,19 +2,19 @@
 /* ==========Модуль для работы с даными из БД=========== */
 
 var mysql = require('mysql'), //подключение библиотеки для работы с mysql
-  clc = require('cli-color'), //подключение библиотеки для выдления цветом в терминале
-  Q = require('q'), //подклчение бибилиотеки для работы с promise
-  moment = require('moment'), //подключение momentjs
-  _ = require('lodash'), //библиотека для работы с массивами
-  async = require('async'); //библиотека для асинхронной работы
+    clc = require('cli-color'), //подключение библиотеки для выдления цветом в терминале
+    Q = require('q'), //подклчение бибилиотеки для работы с promise
+    moment = require('moment'), //подключение momentjs
+    _ = require('lodash'), //библиотека для работы с массивами
+    async = require('async'); //библиотека для асинхронной работы
 moment.locale('ru'); //указание локации у moment js
 
 /* указываем параметры соединения с БД */
 var DB = mysql.createConnection({
-  host: '10.50.0.127', //адрес сервера БД
-  user: 'userScales', //имя пользователя БД
-  password: 'scales', //пароль пользователя БД
-  database: 'scales' //имя БД
+    host: '10.50.0.127', //адрес сервера БД
+    user: 'userScales', //имя пользователя БД
+    password: 'scales', //пароль пользователя БД
+    database: 'scales' //имя БД
 });
 
 const arrTypeScales = ['brutto', 'netto']; //создание массива с типоами весов
@@ -25,122 +25,122 @@ handleDisconnect(DB); //поддержка постоянного соедине
 
 /* поддержка постоянного соединения с БД */
 function handleDisconnect(client) {
-  client.on('error', error => {
-    console.log(error)
-    if (!error.fatal) return;
-    if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
-    DB = mysql.createConnection(client.config);
-    handleDisconnect(DB);
-    DB.connect();
-  });
+    client.on('error', error => {
+        console.log(error)
+        if (!error.fatal) return;
+        if (error.code !== 'PROTOCOL_CONNECTION_LOST') throw err;
+        DB = mysql.createConnection(client.config);
+        handleDisconnect(DB);
+        DB.connect();
+    });
 }
 
 /* обработка ошибок запросов */
 function DefineError(err, FuncName) {
-  if (err)
-    throw console.log(
-      clc.red(
-        moment().format('LLL') +
-        ' Произошла ошибка:' +
-        err +
-        '\n' +
-        'в функции: ' +
-        FuncName
-      )
-    );
+    if (err)
+        throw console.log(
+            clc.red(
+                moment().format('LLL') +
+                ' Произошла ошибка:' +
+                err +
+                '\n' +
+                'в функции: ' +
+                FuncName
+            )
+        );
 }
 
-/* получение емен весов */
+/* получение имен весов */
 exports.GetNameScales = callback => {
-  var sql = 'SELECT DISTINCT name FROM AdressScales'; //формирование звапроса
-  DB.query(sql, (err, rows) => { //выполнение запроса
-    DefineError(err, 'GetNameScales'); //обрабока ошибок
-    callback(rows); //возвратрезультата в callback
-  })
+    var sql = 'SELECT DISTINCT name FROM AdressScales'; //формирование звапроса
+    DB.query(sql, (err, rows) => { //выполнение запроса
+        DefineError(err, 'GetNameScales'); //обрабока ошибок
+        callback(rows); //возвратрезультата в callback
+    })
 }
 
 //TODO
 /* получение  группированных вагонов у составов по дням на весах*/
-exports.GetSostavGroupOfVagonsForDay = async (params, callback) => {
-  FillArr(params).then(ArrInitial => { //Обход массива типов весов
-    GetArrDate(ArrInitial).then(arrDate => {
-      GetArrDateDay(arrDate)
+exports.GetSostavGroupOfVagonsForDay = async(params, callback) => {
+    FillArr(params).then(ArrInitial => { //Обход массива типов весов
+        GetArrDate(ArrInitial).then(arrDate => {
+            GetArrDateDay(arrDate)
+        })
     })
-  })
 
-  /* обход типов весов */
-  async function FillArr(params) {
-    var result = Q.defer(); //создание promise
-    var arr = []; //новый массив
-    var sql = 'SELECT date_format(DateTimeOp, "%Y-%m-%d %H:%i") as DateTimeOp, MAX(NumVagons) as CountVagons,  SUM(Mass) as Mass FROM DataScales WHERE (DateTimeOp BETWEEN ? AND ?) AND CountWagons>4 AND Scales=? AND typeScales=? GROUP BY DateTimeOp'; //формирование запроса
-    var value = [params.DateTimeStart, params.DateTimeEnd, params.NameScales, 'brutto']
-    sql = await DB.format(sql, value); //формирование sql запроса со значениями для параметров
-    var Obj = {}; //создание нового объекта
-    Obj.NameScales = params.NameScales; //доабвление в объект имени весов
-    Obj.typeScaels = 'brutto'; //добалвние в объект типа весов
-    await ExecuteQery(sql).then(async res => { //выполнение 
-      Obj.List = res; //доабвлекние в объеккт результата запроса
-      await arr.push(Obj); //добавление объекта в массив
-    })
-    await result.resolve(arr); //доабвление 
-    return result.promise; //возврат результата в promise
-  }
+    /* обход типов весов */
+    async function FillArr(params) {
+        var result = Q.defer(); //создание promise
+        var arr = []; //новый массив
+        var sql = 'SELECT date_format(DateTimeOp, "%Y-%m-%d %H:%i") as DateTimeOp, MAX(NumVagons) as CountVagons,  SUM(Mass) as Mass FROM DataScales WHERE (DateTimeOp BETWEEN ? AND ?) AND CountWagons>4 AND Scales=? AND typeScales=? GROUP BY DateTimeOp'; //формирование запроса
+        var value = [params.DateTimeStart, params.DateTimeEnd, params.NameScales, 'brutto']
+        sql = await DB.format(sql, value); //формирование sql запроса со значениями для параметров
+        var Obj = {}; //создание нового объекта
+        Obj.NameScales = params.NameScales; //доабвление в объект имени весов
+        Obj.typeScaels = 'brutto'; //добалвние в объект типа весов
+        await ExecuteQery(sql).then(async res => { //выполнение 
+            Obj.List = res; //доабвлекние в объеккт результата запроса
+            await arr.push(Obj); //добавление объекта в массив
+        })
+        await result.resolve(arr); //доабвление 
+        return result.promise; //возврат результата в promise
+    }
 
-  /* выполнение запроса */
-  function ExecuteQery(sql) {
-    var result = Q.defer(); //создание promise
-    DB.query(sql, async (err, rows) => { //выполнение sql зарпоса
-      result.resolve(rows); //доабвление результата в promise
-    })
-    return result.promise; //возврат результата в promise
-  }
+    /* выполнение запроса */
+    function ExecuteQery(sql) {
+        var result = Q.defer(); //создание promise
+        DB.query(sql, async(err, rows) => { //выполнение sql зарпоса
+            result.resolve(rows); //доабвление результата в promise
+        })
+        return result.promise; //возврат результата в promise
+    }
 
-  /* получение массива дат */
-  function GetArrDate(List) {
-    var result = Q.defer(); //создание promise
-    var arrDate = []; //создание масива дляхранения дат
-    var Listdate = List[0].List; //исходный массив дат 
-    Listdate.forEach(async (rowBrutto, indBrutto) => { //обход значений массива
-      await arrDate.push(rowBrutto.DateTimeOp); //добавление в массив строки
-      if (indBrutto == List.length - 1) { //првоерка последней строки
-        result.resolve(arrDate); //добавлние рузльтата в promise
-      }
-    })
-    return result.promise //возврат результата в promise
-  }
+    /* получение массива дат */
+    function GetArrDate(List) {
+        var result = Q.defer(); //создание promise
+        var arrDate = []; //создание масива дляхранения дат
+        var Listdate = List[0].List; //исходный массив дат 
+        Listdate.forEach(async(rowBrutto, indBrutto) => { //обход значений массива
+            await arrDate.push(rowBrutto.DateTimeOp); //добавление в массив строки
+            if (indBrutto == List.length - 1) { //првоерка последней строки
+                result.resolve(arrDate); //добавлние рузльтата в promise
+            }
+        })
+        return result.promise //возврат результата в promise
+    }
 
-  /* получение массива уникальных дней */
-  async function GetArrDateDay(List) {
-    var result = Q.defer(); //создание promise
-    var arrDate = []; //массив для хранения результата
-    await List.forEach(async (row) => { //обход массива
-      var StartDay =  moment(new Date(row)).startOf('day').format('YYYY-MM-DD HH:mm'); //начало дня
-      var EndDay =  moment(new Date(row)).endOf('day').format('YYYY-MM-DD HH:mm'); //начало дня 
-      var ind = arrDate.map(row => {
-        return row.StartDay
-      }).indexOf(StartDay);//поиск в массиве элементов
-      if (ind == -1) {  
-        var Obj = {};
-        Obj.StartDay = StartDay;
-        Obj.EndDay = EndDay;
-        await arrDate.push(Obj)
-        //console.log(StartDay)
-      }
-    })
-    console.log(arrDate)
-  }
-
+    /* получение массива уникальных дней */
+    async function GetArrDateDay(List) {
+        var result = Q.defer(); //создание promise
+        var arrDate = []; //массив для хранения результата
+        await List.forEach(async(row) => { //обход массива
+            var StartDay = moment(new Date(row)).startOf('day').format('YYYY-MM-DD HH:mm'); //начало дня
+            var EndDay = moment(new Date(row)).endOf('day').format('YYYY-MM-DD HH:mm'); //начало дня 
+            var ind = arrDate.map(row => {
+                return row.StartDay
+            }).indexOf(StartDay); //поиск в массиве элементов
+            if (ind == -1) {
+                var Obj = {};
+                Obj.StartDay = StartDay;
+                Obj.EndDay = EndDay;
+                await arrDate.push(Obj)
+                    //console.log(StartDay)
+            }
+        })
+        console.log(arrDate)
+    }
 
 
 
-  /*   var s =arrDate.indexOf(date => {
-      date =  moment(new Date(date)).startOf('day').format('YYYY-MM-DD HH:mm');
-      date == StartDay;
-      console.log('ss')
-    }) */
+
+    /*   var s =arrDate.indexOf(date => {
+        date =  moment(new Date(date)).startOf('day').format('YYYY-MM-DD HH:mm');
+        date == StartDay;
+        console.log('ss')
+      }) */
 
 
-  /*     ArrDataOfScales.forEach(async row => { //ассинхронный обход массива 
+    /*     ArrDataOfScales.forEach(async row => { //ассинхронный обход массива 
       NameScales = row.NameScales; //имя весов
       let typeScaels = row.typeScaels; //объявляем тип весов
       arrBrutto = row.List; //присвоние данных массиву
@@ -161,34 +161,11 @@ exports.GetSostavGroupOfVagonsForDay = async (params, callback) => {
  */
 
 
-  /*       arrNetto.filter(rowNetto => {
-          var Obj = {};//создание объекта для хранения данных
-          var DateOpNetto = moment(rowNetto.DateTimeOp).format('YYYY-MM-DD HH:mm'); //дата операции Нетто
-          var CountVagonsNetto = rowNetto.CountVagons; //количество вагонов в составе Нетто
-          var MassNetto = rowNetto.Mass; //масса состава Нетто
-          for (var i = 2; i <= 12; i++) { //цикл для увеличения времени
-            var FindDateTime = moment(DateOpNetto).add(i, 'minutes').format('YYYY-MM-DD HH:mm');
-            if((FindDateTime == DateOpBrutto) && (CountVagonsNetto == CountVagonsBrutto)){
-              Obj.DatetimeOp = DateOpBrutto;
-              Obj.MassBrutto = MassBrutto;
-              Obj.MasNetto = MassNetto;
-              arrConformity.push(Obj);
-              console.log(DateOpBrutto)
-              Status = true;
-              break;
-            }
-          }
-        }) */
-
-
-
-
-  /*        arrNetto.forEach(async (rowNetto, indNetto) => {
+    /*       arrNetto.filter(rowNetto => {
             var Obj = {};//создание объекта для хранения данных
             var DateOpNetto = moment(rowNetto.DateTimeOp).format('YYYY-MM-DD HH:mm'); //дата операции Нетто
             var CountVagonsNetto = rowNetto.CountVagons; //количество вагонов в составе Нетто
             var MassNetto = rowNetto.Mass; //масса состава Нетто
-            
             for (var i = 2; i <= 12; i++) { //цикл для увеличения времени
               var FindDateTime = moment(DateOpNetto).add(i, 'minutes').format('YYYY-MM-DD HH:mm');
               if((FindDateTime == DateOpBrutto) && (CountVagonsNetto == CountVagonsBrutto)){
@@ -201,8 +178,31 @@ exports.GetSostavGroupOfVagonsForDay = async (params, callback) => {
                 break;
               }
             }
-          })  
-  }) */
-  //callback(arrConformity)
+          }) */
+
+
+
+
+    /*        arrNetto.forEach(async (rowNetto, indNetto) => {
+              var Obj = {};//создание объекта для хранения данных
+              var DateOpNetto = moment(rowNetto.DateTimeOp).format('YYYY-MM-DD HH:mm'); //дата операции Нетто
+              var CountVagonsNetto = rowNetto.CountVagons; //количество вагонов в составе Нетто
+              var MassNetto = rowNetto.Mass; //масса состава Нетто
+              
+              for (var i = 2; i <= 12; i++) { //цикл для увеличения времени
+                var FindDateTime = moment(DateOpNetto).add(i, 'minutes').format('YYYY-MM-DD HH:mm');
+                if((FindDateTime == DateOpBrutto) && (CountVagonsNetto == CountVagonsBrutto)){
+                  Obj.DatetimeOp = DateOpBrutto;
+                  Obj.MassBrutto = MassBrutto;
+                  Obj.MasNetto = MassNetto;
+                  arrConformity.push(Obj);
+                  console.log(DateOpBrutto)
+                  Status = true;
+                  break;
+                }
+              }
+            })  
+    }) */
+    //callback(arrConformity)
 
 }
--- public/js/function.js
+++ public/js/function.js
@@ -6,154 +6,163 @@ $.datetimepicker.setLocale('ru'); //установка локации для dat
 
 /* при загрузке страницы */
 $(window).on('load', () => {
-  $('#Data').show('', () => {
-    $('#ListScalesDataMass').height(GetHeightListScalesDataMass() - 64); //установка высоты для блока ListScalesDataMass
-  });
-  $('.loader').hide(); //скрыть элемент
-  OnLoadIndex(); //после заггрузки главной страницы
-  FillDateTimemainGraphics(); //заполнение дат
-})
+	$('#Data').show('', () => {
+		$('#ListScalesDataMass').height(GetHeightListScalesDataMass() - 64); //установка высоты для блока ListScalesDataMass
+	});
+	$('.loader').hide(); //скрыть элемент
+	OnLoadIndex(); //после заггрузки главной страницы
+	FillDateTimemainGraphics(); //заполнение дат
+});
 
 /* нажатие на кнопку "График" */
 $('#ItemMainNav_Graphics').on('click', LoadFormGraphics()); //при нажитии на кнопку "Применить" - загрузить форму с графиком
 
 /* заполнение дат начала и текущию дату в гавном кграфике */
 function FillDateTimemainGraphics() {
-  /* получение даты начала месяца */
-  function GetStartDate() {
-    var date = moment().add(-1, 'M').startOf('month').format('DD.MM.YYYY HH:mm'); //установка формата времени moment
-    return date; //возврат значения
-  }
-  var datetimeStart = GetStartDate(); //начало месяца
+	/* получение даты начала месяца */
+	function GetStartDate() {
+		var date = moment()
+			.add(-1, 'M')
+			.startOf('month')
+			.format('DD.MM.YYYY HH:mm'); //установка формата времени moment
+		return date; //возврат значения
+	}
+	var datetimeStart = GetStartDate(); //начало месяца
 
-  /* получение текущей даты */
-  function GetEndtDate() {
+	/* получение текущей даты */
+	function GetEndtDate() {
+		var date = moment().format('DD.MM.YYYY HH:mm'); //установка даты в нудном формате
+		return date; //возврат значения
+	}
+	var datetimeEnd = GetEndtDate(); //текущая дата
 
-    var date = moment().format('DD.MM.YYYY HH:mm'); //установка даты в нудном формате
-    return date; //возврат значения
-  }
-  var datetimeEnd = GetEndtDate(); //текущая дата
+	$('#datetimeStart').val(datetimeStart); //заполнение поля
+	$('#datetimeStart')
+		.next()
+		.each((ind, el) => {
+			//перебор элементов
+			$(el).addClass('active'); //добавление класса для label
+		});
+	$('#datetimeEnd').val(datetimeEnd);
+	$('#datetimeEnd')
+		.next()
+		.each((ind, el) => {
+			//перебор элементов
+			$(el).addClass('active'); //добавление класса для label
+		});
 
-  $('#datetimeStart').val(datetimeStart); //заполнение поля
-  $('#datetimeStart').next().each((ind, el) => { //перебор элементов
-    $(el).addClass('active') //добавление класса для label
-  })
-  $('#datetimeEnd').val(datetimeEnd);
-  $('#datetimeEnd').next().each((ind, el) => { //перебор элементов
-    $(el).addClass('active') //добавление класса для label
-  })
+	/* установка типа datetimepicker для input */
+	function SetDatePimePicker() {
+		$('#datetimeStart').datetimepicker({
+			timepicker: true,
+			format: 'Y.m.d H:i',
+		}); //указываем что это datepicker
+		$('#datetimeEnd').datetimepicker({
+			timepicker: true,
+			format: 'Y.m.d H:i',
+		}); //указываем что это datepicker
+	}
 
-  /* установка типа datetimepicker для input */
-  function SetDatePimePicker() {
-    $('#datetimeStart').datetimepicker({
-      timepicker: true,
-      format: 'Y.m.d H:i',
-    }); //указываем что это datepicker
-    $('#datetimeEnd').datetimepicker({
-      timepicker: true,
-      format: 'Y.m.d H:i',
-    }); //указываем что это datepicker
-  }
-
-  SetDatePimePicker(); //установка datetimepicker для полей input
+	SetDatePimePicker(); //установка datetimepicker для полей input
 }
 
 /* определение высоты #ListScalesDataMass */
 function GetHeightListScalesDataMass() {
-  var result = 0; //результат
-  var HeightNavigate = 0; //высота навигации
-  var HeightFooter = 0; //высота футера
-  var HeightBody = 0; //получение высоты окна
+	var result = 0; //результат
+	var HeightNavigate = 0; //высота навигации
+	var HeightFooter = 0; //высота футера
+	var HeightBody = 0; //получение высоты окна
 
-  /* получение высота меню навигации */
-  function GetHeightNavigate() {
-    var height = $('#MainNavigateMenu').height(); //получение высоты элемента
-    return height; //возврат результата
-  }
-  HeightNavigate = GetHeightNavigate(); //высота блока навигации
+	/* получение высота меню навигации */
+	function GetHeightNavigate() {
+		var height = $('#MainNavigateMenu').height(); //получение высоты элемента
+		return height; //возврат результата
+	}
+	HeightNavigate = GetHeightNavigate(); //высота блока навигации
 
-  /* получение высоты footer */
-  function GetHeightFooter() {
-    var height = $('.page-footer').height(); //получение высоты футера
-    return height; //возрат результата
-  }
-  HeightFooter = GetHeightFooter(); //высота блока footer
+	/* получение высоты footer */
+	function GetHeightFooter() {
+		var height = $('.page-footer').height(); //получение высоты футера
+		return height; //возрат результата
+	}
+	HeightFooter = GetHeightFooter(); //высота блока footer
 
-  /* получение высоты body */
-  function GetHeightBody() {
-    var height = $('body').height(); //получение высоты body
-    return height; //возврат результата
-  }
-  HeightBody = GetHeightBody(); //высота страницы
+	/* получение высоты body */
+	function GetHeightBody() {
+		var height = $('body').height(); //получение высоты body
+		return height; //возврат результата
+	}
+	HeightBody = GetHeightBody(); //высота страницы
 
-  result = HeightBody - (HeightNavigate + HeightFooter); //получение высоты без учета блоков
+	result = HeightBody - (HeightNavigate + HeightFooter); //получение высоты без учета блоков
 
-  return result; //возврат результата
+	return result; //возврат результата
 }
 
 /* после заггрузки главной страницы */
 function OnLoadIndex() {
-  $('#ItemMainNav_Graphics').parent('li').addClass('active'); //добавление класса элементу
-  $('#ItemMainNav_Graphics').click(); //нажатие кнопки
-  $(window).on('resize', () => { //при изменнеии размеров формы
-    $('#ListScalesDataMass').height(GetHeightListScalesDataMass() - 64); //установка высоты для блока ListScalesDataMass
-  })
-  FillCardsScales(); //заполнение карточек весов
+	$('#ItemMainNav_Graphics')
+		.parent('li')
+		.addClass('active'); //добавление класса элементу
+	$('#ItemMainNav_Graphics').click(); //нажатие кнопки
+	$(window).on('resize', () => {
+		//при изменнеии размеров формы
+		$('#ListScalesDataMass').height(GetHeightListScalesDataMass() - 64); //установка высоты для блока ListScalesDataMass
+	});
+	FillCardsScales(); //заполнение карточек весов
 }
 
 /* загрузка формы Графики */
 function LoadFormGraphics() {
-  $('#MainData').load('public/Forms/mainChart.html', () => { //загрузка файла html
-    $('#ListScalesDataMass').height(GetHeightListScalesDataMass() - 64); //установка высоты для блока ListScalesDataMass
-  })
+	$('#MainData').load('public/Forms/mainChart.html', () => {
+		//загрузка файла html
+		$('#ListScalesDataMass').height(GetHeightListScalesDataMass() - 64); //установка высоты для блока ListScalesDataMass
+	});
 }
 
 /* заполнение каточек весов */
 function FillCardsScales() {
+	$('#ListScalesDataMass .scrollbar-primary').empty(); //очистка блока
+	socket.emit('GetNameScales', list => {
+		//получение имен весов
+		list.forEach((Scales, ind) => {
+			//перебор полученных значений
+			var NameScales = Scales['name']; //имя весов
+			GetSostavGroupOfVagonsForDay(NameScales);
+			Card(NameScales, ind); //формирование карточки весов
+		});
+	});
 
-  $('#ListScalesDataMass .scrollbar-primary').empty(); //очистка блока
-  socket.emit('GetNameScales', list => { //получение имен весов
-    list.forEach((Scales, ind) => { //перебор полученных значений
-      var NameScales = Scales['name']; //имя весов
-      GetSostavGroupOfVagonsForDay(NameScales);
-      Card(NameScales, ind); //формирование карточки весов
-    })
-  })
+	/* ганерания карточки */
+	function Card(NameScales, ind) {
+		$('<div>', {
+			class: 'col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12',
+			style: 'padding-top: 3%;',
+		}).appendTo('#ListScalesDataMass .scrollbar-primary'); //создание блока
 
-  /* ганерания карточки */
-  function Card(NameScales, ind) {
-    $('<div>', {
-        class: "col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12",
-        style: "padding-top: 3%;"
-      })
-      .appendTo('#ListScalesDataMass .scrollbar-primary'); //создание блока
+		$('<div>', {
+			class: 'card',
+		}).appendTo($('#ListScalesDataMass .scrollbar-primary .col-xs-12')[ind]); //создание блока
 
-    $('<div>', {
-        class: "card"
-      })
-      .appendTo($('#ListScalesDataMass .scrollbar-primary .col-xs-12')[ind]); //создание блока
+		$('<div>', {
+			class: 'card-body',
+		}).appendTo($('.scrollbar-primary  .card')[ind]); //создание блока
 
-    $('<div>', {
-        class: "card-body"
-      })
-      .appendTo($('.scrollbar-primary  .card')[ind]); //создание блока
+		$('<h4>', {
+			class: 'card-title',
+			text: NameScales,
+		}).appendTo($('.scrollbar-primary  .card-body')[ind]); //создание заголовка с именем весов
+	}
 
-    $('<h4>', {
-        class: "card-title",
-        text: NameScales
-      })
-      .appendTo($('.scrollbar-primary  .card-body')[ind]); //создание заголовка с именем весов
-  }
-
-  /* получение массы добычи */
-  function GetSostavGroupOfVagonsForDay(NameScales){
-    var params = {};  //создание объекта для хранения параметров
-    params.DateTimeStart = moment(new Date($('#datetimeStart').val())).format('YYYY-MM-DD HH:mm');  //дата начала 
-    params.DateTimeEnd = moment(new Date($('#datetimeEnd').val())).format('YYYY-MM-DD HH:mm');  //дата окончания
-    params.NameScales = NameScales; //имя весов 
-    socket.emit('GetSostavGroupOfVagonsForDay', params, result => { //отправить запрос через socket
-      console.log(result)
-    })
-  }
-
+	/* получение массы добычи */
+	function GetSostavGroupOfVagonsForDay(NameScales) {
+		var params = {}; //создание объекта для хранения параметров
+		params.DateTimeStart = moment(new Date($('#datetimeStart').val())).format('YYYY-MM-DD HH:mm'); //дата начала
+		params.DateTimeEnd = moment(new Date($('#datetimeEnd').val())).format('YYYY-MM-DD HH:mm'); //дата окончания
+		params.NameScales = NameScales; //имя весов
+		socket.emit('GetSostavGroupOfVagonsForDay', params, result => {
+			//отправить запрос через socket
+			console.log(result);
+		});
+	}
 }
